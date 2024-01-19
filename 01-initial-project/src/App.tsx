7c9fc6a1d681364cc73f893867b0d6048846017c
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-18 23:09:54 -0800

    Added props ⅊

--- 01-initial-project/src/App.tsx
+++ 01-initial-project/src/App.tsx
@@ -1,3 +1,12 @@
+import reactImage from './assets/react-core-concepts.png';
+import componentImage from './assets/components.png';
+
+interface CoreConceptProps {
+    image: string;
+    title: string;
+    description: string;
+}
+
 const reactDescriptions: string[] = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max: number): number {
@@ -7,7 +16,7 @@ function genRandomInt(max: number): number {
 function Header() {
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src={reactImage} alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
                 {reactDescriptions[genRandomInt(2)]}
@@ -16,11 +25,32 @@ function Header() {
     );
 }
 
+function CoreConcept(props: CoreConceptProps) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3> {props.title} </h3>
+            <p> {props.description} </p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
+                <section id='core-concepts'>
+                    <h2> Core concept</h2>
+                    <ul>
+                        <CoreConcept title="Components"
+                            description="Core UI"
+                            image={componentImage} />
+                        <CoreConcept title="" description="" image="" />
+                        <CoreConcept title="" description="" image="" />
+                        <CoreConcept title="" description="" image="" />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>

9cdf5ff906e4e693732b55da1d82f836ea39fcdd
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-18 18:11:02 -0800

    Added dynamic content 💨

--- 01-initial-project/src/App.tsx
+++ 01-initial-project/src/App.tsx
@@ -1,11 +1,16 @@
+const reactDescriptions: string[] = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max: number): number {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
-                going to build!
+                {reactDescriptions[genRandomInt(2)]}
             </p>
         </header>
     );
@@ -18,7 +23,7 @@ function App() {
             <main>
                 <h2>Time to get started!</h2>
             </main>
-        </div >
+        </div>
     );
 }
 

a54ccc0cf12fe9d81791c50562e93a2ae3b2fa47
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-08 23:14:12 -0800

    Folder structure changed

--- 01-initial-project/src/App.tsx
+++ 01-initial-project/src/App.tsx
@@ -1,19 +1,20 @@
-import { CORE_CONCEPTS } from './data';
-import Header from './components/Header';
-import CoreConcept from './components/CoreConcepts';
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcepts";
 
 function App() {
     return (
         <div>
             <Header />
             <main>
-                <section id='core-concepts'>
+                <section id="core-concepts">
                     <h2> Core concept</h2>
                     <ul>
-
-                        <CoreConcept title={CORE_CONCEPTS[0].title}
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
                             description={CORE_CONCEPTS[0].description}
-                            image={CORE_CONCEPTS[0].image} />
+                            image={CORE_CONCEPTS[0].image}
+                        />
 
                         {/* Another Way of representing Props is as below */}
                         <CoreConcept {...CORE_CONCEPTS[1]} />
--- 01-initial-project/src/components/Header/Header.tsx
+++ 01-initial-project/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import reactImage from '../assets/react-core-concepts.png';
-const reactDescriptions: string[] = ['Fundamental', 'Crucial', 'Core'];
+import reactImage from "../../assets/react-core-concepts.png";
+import "./Header.css";
+const reactDescriptions: string[] = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max: number): number {
     return Math.floor(Math.random() * (max + 1));
@@ -10,9 +11,7 @@ export default function Header() {
         <header>
             <img src={reactImage} alt="Stylized atom" />
             <h1>React Essentials</h1>
-            <p>
-                {reactDescriptions[genRandomInt(2)]}
-            </p>
+            <p>{reactDescriptions[genRandomInt(2)]}</p>
         </header>
     );
 }

7163123d135e80aa8032851d882096eb1290259d
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-06-11 19:22:10 -0700

    Hello World

--- nodeJS/01-node-server/app.js
+++ nodeJS/01-node-server/app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // console.log(req);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title> Hello World </title></head>");
+    res.write("<body> Hello world </body>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(3000);
